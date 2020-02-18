181f8cd9fa3e2030f303f60fcc12bee4ce5b13cb
Ash84 <louis.tromel@gmail.com>
2020-02-18 16:42:17 +0100

    Added inputs correctly

--- src/main/java/snake/Game.java
+++ src/main/java/snake/Game.java
@@ -14,6 +14,9 @@ public class Game extends JFrame implements KeyListener {
     int nCols;
     Grid g;
     Snake s;
+    static boolean turnright;
+    static boolean turnleft;
+    static Timer timer;
 
     public Game(String name, int nRows, int nCols) {
         super(name);
@@ -22,18 +25,16 @@ public class Game extends JFrame implements KeyListener {
         this.nCols = nCols;
 
         this.g = new Grid(nRows, nCols);
-        int[] pos = {4, 4};
-        int[] speed = {1, 0};
+        int[] pos = { 2, 1 };
+        int[] speed = { 1, 0 };
         this.s = new Snake(pos, speed, "red", name);
         g.dropBlock(6, 4);
         pack();
         setResizable(false);
         setVisible(true);
 
-        Timer timer = new Timer(1000, null);
-        GridActionListener listener = new GridActionListener(
-                this, g, s, timer
-        );
+        timer = new Timer(1000, null);
+        GridActionListener listener = new GridActionListener(this, g, s, timer);
         timer.addActionListener(listener);
         timer.start();
     }
@@ -44,28 +45,34 @@ public class Game extends JFrame implements KeyListener {
         // Create and set up the window.
         Game frame = new Game("SnakeThis", nRows, nCols);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         frame.pack();
         frame.setVisible(true);
     }
 
     // Satisfy KeyListener interface constraint
-    public void keyReleased(KeyEvent e) { }
-    public void keyTyped(KeyEvent e) { }
+    public void keyReleased(KeyEvent e) {
+    }
+
+    public void keyTyped(KeyEvent e) {
+    }
+
     public void keyPressed(KeyEvent e) {
+
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            s.turnRight();
+            turnright = true;
+            turnleft = false;
         }
         if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            s.turnLeft();
+            turnleft = true;
+            turnright = false;
         }
     }
 
     public static void main(String[] args) {
         // Recover command line arguments for the size of
         // the grid to generate
-        int nRows = Integer.parseInt(args[0]);
-        int nCols = Integer.parseInt(args[1]);
+        int nRows = 10;// Integer.parseInt(args[0]);
+        int nCols = 20;// Integer.parseInt(args[1]);
 
         // Schedule app boot in the EDT
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
@@ -75,7 +82,6 @@ public class Game extends JFrame implements KeyListener {
         });
     }
 }
-
 
 class GridActionListener implements ActionListener {
 
@@ -93,10 +99,25 @@ class GridActionListener implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) {
+
+        // Game sets the last input for a turn, and sets all to false for next tick.
+
+        if (Game.turnright)
+            s.turnRight();
+        if (Game.turnleft)
+            s.turnLeft();
+        Game.turnleft = false;
+        Game.turnright = false;
+        
+        // Tick
+
         s.moveOn(g);
         g.updateGrids(s);
         System.out.println(g);
         this.i++;
+
+        // If snake dies.
+        
         if (!s.isAlive()) {
             try {
                 ScoreDisplay.scoreDisplay(s,i);
---
+++ src/main/java/snake/ScoreDisplay.java
@@ -0,0 +1,61 @@
+package snake;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ScoreDisplay {
+
+    public static void scoreDisplay(Snake s, int i) throws Exception {
+
+        JSONObject Score = new JSONObject();
+        Score.put("Score",s.getSize());
+        Score.put("Turns",i);
+        Score.put("Name",s.getPlayer());
+        JSONArray Scores = new JSONArray();
+
+        if(Files.exists(Paths.get("./Scores.json"))) {
+            Scores = readScores();
+            if (Scores.size() > 0){
+                JSONObject buffer;
+                Boolean betterScore = false;
+                for (int j=Scores.size()-1; j>=0; j--) {
+                    buffer = (JSONObject) Scores.get(j);
+                    String name = (String) buffer.get("Name");
+                    if(name.compareTo(s.getPlayer()) == 0) {
+                        long score = (long) buffer.get("Score");
+                        if(score <= s.getSize()){
+                            Scores.remove(j);
+                            betterScore = true;
+                        }
+                    }
+                }
+                if (betterScore)
+                    Scores.add(Score);
+            }
+            else
+                Scores.add(Score);
+
+        }
+        try {
+            FileWriter ScoreWriter = new FileWriter("./Scores.json");
+            ScoreWriter.write(Scores.toJSONString());
+            ScoreWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("JSON file (re)created: "+ Scores);
+    }
+
+    public static JSONArray readScores() throws Exception {
+        FileReader ScoreReader = new FileReader("./Scores.json");
+        JSONParser parser = new JSONParser();
+        return (JSONArray) parser.parse(ScoreReader);
+    }
+}
